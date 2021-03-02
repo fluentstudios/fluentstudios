1f278fb703bbb32c4c4be2bab0203e7bd9c2d872
fluentstudios <79485923+fluentstudios@users.noreply.github.com>
2021-03-02 14:00:03 +0800

    Update lookup_tool.js

--- js/lookup_tool.js
+++ js/lookup_tool.js
@@ -113,9 +113,11 @@ function addressSearch() {
                     
                     $.each(division.officeIndices, function(i, office){
                         var office_name = offices[office];
-                        
+                        var result_count = 0;
                         $.each(offices[office]['officialIndices'], function(i, official){
-                            
+                            result_count++;
+                            //break the loop, force to show only two results
+                            if(result_count > 2){ console.log('result count'); return false; }
                             var info = {
                                 'person': null,
                                 'office': office_name,
